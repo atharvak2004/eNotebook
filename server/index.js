43ea01ec2cbb8be8abe029ad81a5f120a6f4eec5
atharvak <atharvakadam631@gmail.com>
2025-08-24 18:31:37 +0530

    backend cors

--- server/index.js
+++ server/index.js
@@ -13,9 +13,21 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:5173"];
+const allowedOrigins = [
+  "https://e-notebook-phi.vercel.app", 
+  process.env.CLIENT_URL,             
+  "http://localhost:5173"         
+];
+
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 }));
 
